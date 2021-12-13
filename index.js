a50e99158ef76eb270a7f8d31e2b6eb9edca54eb
sujan <sujankapali50@gmail.com>
2021-12-13 22:05:25 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//Call scrollFunction
+document
+  .getElementById("outer-wrapper")
+  .addEventListener("scroll", stickyFunction);
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyFunction() {
+  // Get the navbar
+  var navbar = document.getElementById("navbar");
+
+  // Get the offset position of the navbar
+  var navPos = navbar.getBoundingClientRect().left;
+
+  console.log(navPos);
+}
